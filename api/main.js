aeed66ed1922e372817e321adb346c57d02407c6
SheogorathD <aknojlogiya@gmail.com>
2022-08-12 00:09:55 +0300

    Trying to use firebase (before using it)

---
+++ api/main.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
