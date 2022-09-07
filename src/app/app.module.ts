98d4d9f9f49e99df6512fb22d8d2bd81e2b273cb
Volodymyr Shcherbyna <aknojlogiya@gmail.com>
2022-09-07 11:54:35 +0300

    all the components templates before start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { ItemComponent } from './components/item/item.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GamesComponent } from './pages/games/games.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+import { SeriesComponent } from './pages/series/series.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    MainNavComponent,
+    ItemComponent,
+    HomeComponent,
+    GamesComponent,
+    MoviesComponent,
+    SeriesComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
